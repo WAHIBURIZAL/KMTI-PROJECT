3edeced1dff9a1fb0046c71b386b65dae1ee496e
Rizal Jauharul Mawahib <rizaljauharul@gmail.com>
2026-05-03 11:26:29 +0700

    Fix: configure whitenoise and static files for deployment

--- kmti/settings.py
+++ kmti/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)y0z5gi5oje!0oy3-a@x)0=+g-b@mg12xx89-xav7xfjv(x2f4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kmti-project.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,9 +122,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Folder tempat Anda menyimpan file aset (logo, css) saat pengembangan
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Folder tujuan saat perintah 'collectstatic' dijalankan (penting untuk Render)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Agar WhiteNoise mengompres file statis untuk performa lebih baik
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
